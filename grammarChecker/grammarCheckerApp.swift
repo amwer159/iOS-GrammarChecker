1aec4d3952b440c8dd7ef26d823ff5ccc36201cb
Viktor Ilin <viktor.ilin@navicogroup.com>
2024-12-30 16:44:01 +0100

    Initial Commit

---
+++ grammarChecker/grammarCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  grammarCheckerApp.swift
+//  grammarChecker
+//
+//  Created by Ilin, Viktor (Contractor) on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct grammarCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
